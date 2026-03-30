26463fcdc976a64dba0bcccdbc6662c03505be0d
Furkan Gülşen <furkangulsenn19@gmail.com>
2026-03-30 15:16:27 +0300

    Add files via upload

---
+++ Antrenman Programı/Program.cs
@@ -0,0 +1,23 @@
+﻿using AntrenmanProgrami;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Antrenman_Programı
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
